e3f49d6695efd602c27cdab1d4c3f00aba2bf9e6
primocodetoday <primocodetoday@gmail.com>
2020-09-25 13:52:29 +0200

    Jumbotron created as compound component pattern

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
 import React from 'react';
-import './App.css';
-import Row from 'components/Row/Row';
-import requests from 'axios/requests';
-import Banner from 'components/Banner/Banner';
-import Nav from 'components/Nav';
+import Jumbotron from 'components/Jumbotron';
+import jumboData from 'fixtures/jumbo.json';
 
 const App = () => {
   return (
     <div className="app">
-      <Nav />
-      <Banner />
-      <section>
-        <Row
-          title="NETFLIX ORIGINALS"
-          fetchUrl={requests.fetchNetflixOriginals}
-          isLargeRow
-        />
-        <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-        <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-        <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-        <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
-      </section>
+      <Jumbotron.Container>
+        {jumboData.map((item) => (
+          <Jumbotron key={item.id} direction={item.direction}>
+            <Jumbotron.Pane>
+              <Jumbotron.Title>{item.title}</Jumbotron.Title>
+              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+            </Jumbotron.Pane>
+            <Jumbotron.Pane>
+              <Jumbotron.Image src={item.image} alt={item.alt} />
+            </Jumbotron.Pane>
+          </Jumbotron>
+        ))}
+      </Jumbotron.Container>
     </div>
   );
 };
---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,49 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Item,
+  Inner,
+  Container,
+  Pane,
+  Title,
+  SubTitle,
+  Image,
+} from './styles/jumbotron';
+
+const Jumbotron = ({ children, direction }, ...restProps) => {
+  return (
+    <Item {...restProps} direction={direction}>
+      <Inner>{children}</Inner>
+    </Item>
+  );
+};
+
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+};
+
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
+  return <Pane {...restProps}>{children}</Pane>;
+};
+
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Jumbotron.SubTitle = function JumboSubtronTitle({ children, ...restProps }) {
+  return <SubTitle {...restProps}>{children}</SubTitle>;
+};
+
+Jumbotron.Image = function JumbotronImage({ ...restProps }) {
+  return <Image {...restProps} />;
+};
+
+Jumbotron.propTypes = {
+  direction: PropTypes.string,
+};
+
+Jumbotron.defaultProps = {
+  direction: 'row',
+};
+
+export default Jumbotron;
