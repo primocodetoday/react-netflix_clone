57672088942e0d9fa11b34121d09a79ef9d920d0
primocodetoday <primocodetoday@gmail.com>
2020-10-03 22:38:30 +0200

    Firebase connected

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-
 import { Home, Browse, SignIn, SignUp } from 'views';
-
 import GlobalStyles from 'global-styles';
 import 'normalize.css';
-
 import routes from 'routes';
+import { FirebaseContext } from 'context/firebase';
+import firebase from 'firebase/init';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <GlobalStyles />
-      <Route exact path={routes.home} component={Home} />
-      <Route path={routes.browse} component={Browse} />
-      <Route path={routes.signIn} component={SignIn} />
-      <Route path={routes.signUp} component={SignUp} />
-    </BrowserRouter>
+    <FirebaseContext.Provider value={{ firebase }}>
+      <BrowserRouter>
+        <GlobalStyles />
+        <Route exact path={routes.home} component={Home} />
+        <Route path={routes.browse} component={Browse} />
+        <Route path={routes.signIn} component={SignIn} />
+        <Route path={routes.signUp} component={SignUp} />
+      </BrowserRouter>
+    </FirebaseContext.Provider>
   );
 };
 
