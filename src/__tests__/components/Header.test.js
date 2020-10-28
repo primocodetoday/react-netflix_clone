de3e15ebca3a6ed58b12fdbd9b6531616eceb844
primocodetoday <primocodetoday@gmail.com>
2020-10-28 23:01:17 +0100

    test: Profiles component

---
+++ src/__tests__/components/Header.test.js
@@ -0,0 +1,7 @@
+﻿// import React, { useState } from 'react';
+// import { render } from '@testing-library/react';
+// import { Header } from 'components';
+
+// jest.mock('react-router-dom');
+
+// describe('<Header/> renders properly', () => {});
---
+++ src/__tests__/components/Profiles.test.js
@@ -0,0 +1,32 @@
+﻿import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Profiles } from 'components';
+
+describe('<Profiles/> renders properly', () => {
+  const user = {
+    displayName: 'Sebastian',
+  };
+
+  it('with data', () => {
+    const { container } = render(
+      <Profiles>
+        <Profiles.Title>Who&apos;s watching?</Profiles.Title>
+        <Profiles.List>
+          <Profiles.Item onClick={() => {}}>
+            <Profiles.Avatar
+              src="assets/images/fake.png"
+              data-testid="profiles-avatar"
+            />
+            <Profiles.Avatar data-testid="profiles-loading" />
+            <Profiles.Name>{user.displayName}</Profiles.Name>
+          </Profiles.Item>
+        </Profiles.List>
+      </Profiles>,
+    );
+    expect(screen.getByText("Who's watching?")).toBeTruthy();
+    expect(screen.getByText('Sebastian')).toBeTruthy();
+    expect(screen.getAllByTestId('profiles-avatar')).toBeTruthy();
+    expect(screen.getAllByTestId('profiles-loading')).toBeTruthy();
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
