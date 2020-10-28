1ce36b6533fe31df4b4d8cf3407bdcac07fd4d95
primocodetoday <primocodetoday@gmail.com>
2020-10-28 23:19:13 +0100

    test: deleted unused tests

--- src/__tests__/components/Header.test.js
+++
@@ -1,7 +0,0 @@
-﻿// import React, { useState } from 'react';
-// import { render } from '@testing-library/react';
-// import { Header } from 'components';
-
-// jest.mock('react-router-dom');
-
-// describe('<Header/> renders properly', () => {});
